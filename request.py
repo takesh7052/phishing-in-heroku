3c1b803a1e1b60ca2ba4fa270ded939c6cf477e8
takesh7052 <68686658+takesh7052@users.noreply.github.com>
2022-06-26 16:03:27 +0530

    update localhost to 0.0.0.0

--- request.py
+++ request.py
@@ -1,6 +1,8 @@
 import requests
 
-url = 'http://localhost:5000/predict_api'
+# url = 'http://localhost:5000/predict_api'
+url = 'http://0.0.0.0:5000/predict_api'
+
 r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
 
 print(r.json())
