3171466ef258c37533bb6225420131f5b24e47e8
takesh7052 <68686658+takesh7052@users.noreply.github.com>
2022-06-28 12:45:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict():
     return render_template('output.html', accuracy=metric)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
